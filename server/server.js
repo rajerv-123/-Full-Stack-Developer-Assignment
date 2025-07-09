536d1037007ed81b96e5ab14f35956a477893104
RajeevPatel <rajeev.patel@areteminds.com>
2025-07-09 13:31:08 +0530

    commit

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import serverless from "serverless-http";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config.js";
@@ -8,8 +9,9 @@ import profileRoutes from "./routes/profile.js";
 import projectRoutes from "./routes/projects.js";
 
 dotenv.config();
-const app = express();
 connectDB();
+
+const app = express();
 
 app.use(cors());
 app.use(express.json());
@@ -17,8 +19,6 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/profile", profileRoutes);
 app.use("/api/projects", projectRoutes);
-
 app.get("/", (req, res) => res.send("DevConnect API Running"));
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+export const handler = serverless(app);
